d44907673bf0bd5e9c4271c28c36acc1a558677d
Fred <fgaringo@ylopo.com>
2024-01-19 05:29:36 +0800

    used layout

--- src/pages/animal/Animalpage.tsx
+++ src/pages/animal/Animalpage.tsx
@@ -1,7 +1,15 @@
 import React from 'react';
+import PageTitle from '../../components/PageTitle/PageTitle';
+import { Link } from 'react-router-dom';
+import DefaultLayout from '../../layouts/DefaultLayout';
 
 const Animalpage: React.FC = () => {
-	return <div>Animalpage</div>;
+	return (
+		<DefaultLayout>
+			<PageTitle title="Animal" />
+			<Link to="/">Go back</Link>
+		</DefaultLayout>
+	);
 };
 
 export default Animalpage;
--- src/pages/home/Homepage.tsx
+++ src/pages/home/Homepage.tsx
@@ -1,13 +1,16 @@
 import { Button } from 'antd';
 import React from 'react';
 import { Link } from 'react-router-dom';
+import PageTitle from '../../components/PageTitle/PageTitle';
+import DefaultLayout from '../../layouts/DefaultLayout';
 
 const Homepage: React.FC = () => {
 	return (
-		<div>
+		<DefaultLayout>
+			<PageTitle title="Home" />
 			<Button type="primary">Hello World</Button>
 			<Link to="/animal">Go to Animal</Link>
-		</div>
+		</DefaultLayout>
 	);
 };
 
