ac48c3e43b99bb6a4c7e45d47a572ec12bb623df
Fred <fgaringo@ylopo.com>
2024-01-19 08:34:40 +0800

    init navbar content

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,7 +1,26 @@
+import { Menu } from 'antd';
 import React from 'react';
 
 const Navbar: React.FC = () => {
-	return <div>Navbar</div>;
+	const items = [
+		{
+			key: 1,
+			label: 'Home'
+		}
+	];
+
+	return (
+		<div className="navbar">
+			<div className="demo-logo" />
+			<Menu
+				theme="dark"
+				mode="horizontal"
+				defaultSelectedKeys={['1']}
+				items={items}
+				style={{ flex: 1, minWidth: 0 }}
+			/>
+		</div>
+	);
 };
 
 export default Navbar;
