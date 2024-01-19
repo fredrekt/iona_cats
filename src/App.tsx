4936fa008655c28ad5c6d05a3cd436e8878ee9b2
Fred <fgaringo@ylopo.com>
2024-01-19 08:50:30 +0800

    now using context store

--- src/App.tsx
+++ src/App.tsx
@@ -3,15 +3,18 @@ import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
 import Homepage from './pages/home/Homepage';
 import Animalpage from './pages/animal/Animalpage';
 import './api/config';
+import { StoreProvider } from './utils/contextStore';
 
 const App: React.FC = () => {
 	return (
-		<Router>
-			<Routes>
-				<Route path="/" index element={<Homepage />} />
-				<Route path="/animal/:id" element={<Animalpage />} />
-			</Routes>
-		</Router>
+		<StoreProvider>
+			<Router>
+				<Routes>
+					<Route path="/" index element={<Homepage />} />
+					<Route path="/animal/:id" element={<Animalpage />} />
+				</Routes>
+			</Router>
+		</StoreProvider>
 	);
 };
 
