ac08c0d74611c15273f03832505ef608908f253c
Fred <fgaringo@ylopo.com>
2024-01-19 09:55:39 +0800

    updated navbar design

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,24 +1,43 @@
-import { Menu } from 'antd';
+import { Menu, Typography } from 'antd';
 import React from 'react';
+import './Navbar.scss';
+import { useNavigate, useLocation } from 'react-router-dom';
 
 const Navbar: React.FC = () => {
+	const navigate = useNavigate();
+	const location = useLocation();
+
 	const items = [
 		{
-			key: 1,
-			label: 'Home'
+			key: 'home',
+			label: 'Home',
+			path: '/'
 		}
+		// Add more menu items as needed
 	];
+
+	// Extract the pathname from the location object
+	const pathname = location.pathname;
+
+	const handleClick = (path: string) => {
+		navigate(path);
+	};
 
 	return (
 		<div className="navbar">
-			<div className="demo-logo" />
+			<Typography.Text className="navbarLogo">Cats.</Typography.Text>
 			<Menu
-				theme="dark"
+				selectedKeys={[pathname]}
+				theme="light"
 				mode="horizontal"
-				defaultSelectedKeys={['1']}
-				items={items}
-				style={{ flex: 1, minWidth: 0 }}
-			/>
+				style={{ flex: 1, minWidth: 0, background: '#fff', fontSize: 15 }}
+			>
+				{items.map((item) => (
+					<Menu.Item key={item.path} onClick={() => handleClick(item.path)}>
+						{item.label}
+					</Menu.Item>
+				))}
+			</Menu>
 		</div>
 	);
 };
