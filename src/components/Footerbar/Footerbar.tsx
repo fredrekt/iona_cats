fb808da83c5c1bc11ff45158b0bd7622136a6576
Fred <fgaringo@ylopo.com>
2024-01-19 09:11:10 +0800

    added personal link

--- src/components/Footerbar/Footerbar.tsx
+++ src/components/Footerbar/Footerbar.tsx
@@ -3,7 +3,14 @@ import React from 'react';
 import './Footerbar.scss';
 
 const Footerbar: React.FC = () => {
-	return <Typography.Paragraph className="footerTxt"> Created with ❤️ by Fred Garingo</Typography.Paragraph>;
+	return (
+		<Typography.Paragraph className="footerTxt">
+			Created with ❤️ by{' '}
+			<a rel="noreferrer" href="https://fredgaringo-main.netlify.app/" target="_blank">
+				Fred Garingo
+			</a>
+		</Typography.Paragraph>
+	);
 };
 
 export default Footerbar;
