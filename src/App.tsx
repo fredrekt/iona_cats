d396b81b2dc27de3d741229ecde10aef6a7e4b0c
Fred <fgaringo@ylopo.com>
2024-01-19 08:30:51 +0800

    added api config & route params

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,14 @@ import React from 'react';
 import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
 import Homepage from './pages/home/Homepage';
 import Animalpage from './pages/animal/Animalpage';
+import './api/config';
 
 const App: React.FC = () => {
 	return (
 		<Router>
 			<Routes>
 				<Route path="/" index element={<Homepage />} />
-				<Route path="/animal" element={<Animalpage />} />
+				<Route path="/animal/:id" element={<Animalpage />} />
 			</Routes>
 		</Router>
 	);
