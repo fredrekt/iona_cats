544099c8ee7eb5e5066bd12e1257b66dbce0242a
Fred <fgaringo@ylopo.com>
2024-01-19 08:31:37 +0800

    added reusable cat card component

---
+++ src/components/CatCard/CatCard.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import './CatCard.scss';
+import { Animal } from '../../types/animal.types';
+import { Button, Card } from 'antd';
+import { Link } from 'react-router-dom';
+
+interface CatCardProps {
+	cat: Animal;
+}
+
+const CatCard: React.FC<CatCardProps> = ({ cat }) => {
+	return (
+		<Card
+			className="catCard"
+			actions={[
+				<Link to={`/animal/${cat.id}`}>
+					<Button type="primary">View details</Button>
+				</Link>
+			]}
+			hoverable
+			key={cat.id}
+			cover={<img className="catCardPreview" alt="cat" src={cat.url} />}
+		/>
+	);
+};
+
+export default CatCard;
